1becbd2b6f4f0bd2275a66708c08f0c0c307df08
Bryan Yunis <bbyunis@gmail.com>
2018-10-25 15:13:18 -0600

    footer style

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -3,13 +3,11 @@
   <div id = "footer-container">
 <a href = 'https://www.linkedin.com/in/bryan-yunis' target="_blank">
 <img src = "@/assets/images/linkedin.png" class = "embed-images" id = "linkedin-img" /></a>
+<p id='contact-text'>&copy; Bryan Yunis<br> Denver, CO 80209</p>
 <a href = 'https://github.com/bry-an' target="_blank">
 <img src = "@/assets/images/github.png" class = "embed-images" id = "github-img" /></a>
-<a href = 'https://twitter.com/bbyunis?lang=en' target="_blank">
-<img src = "@/assets/images/twitter.png" class = "embed-images" id = "twitter-img" /></a>
-
-<p></p>
-<p id='contact-text'>&copy; Bryan Yunis<br> Denver, CO 80209</p>
+<!-- <a href = 'https://twitter.com/bbyunis?lang=en' target="_blank">
+<img src = "@/assets/images/twitter.png" class = "embed-images" id = "twitter-img" /></a> -->
   </div>
   </template>
     
@@ -49,6 +47,5 @@ a {
 #contact-text {
   text-align: center;
   color: white;
-  
 }
 </style>
--- src/data/portfolioItems.js
+++ src/data/portfolioItems.js
@@ -3,8 +3,8 @@ const portfolioItems =
         {
             name: 'WeatherRider',
             gif: '/static/weather-rider-demo.gif',
-            still:'/static/weather-rider-demo-still.png',
-            problem: "Biking apps don't give users detailed weather at points along their route at a certain time in the future. They also lack headwind information, an important metric for cyclists.", 
+            still: '/static/weather-rider-demo-still.png',
+            problem: "Biking apps don't give users detailed weather at points along their route at a certain time in the future. They also lack headwind information, an important metric for cyclists.",
             solution: 'Enter your start and end points and time of ride, and WeatherRider displays an interactive map with weather information along the route. Users can click any point on the map for weather at that point at the selected time.',
             technologies: ['/static/google-logo.png', '/static/javascript-logo.png', '/static/jquery-logo.png'],
             githubLink: 'https://github.com/bry-an/weather-rider',
@@ -14,23 +14,23 @@ const portfolioItems =
         {
             name: 'NYT Clean',
             gif: '/static/times-react-demo.gif',
-            still:'/static/times-react-demo-still.png',
-            problem: "Most news sites don't provide a clean interface for searching articles and saving them to read at the user's convenience.", 
-            solution: "Readers can search for New York Times articles - with an optional date selection - see the results, and save articles for later, all in a snappy clutter-free interface on NYT Clean.",
+            still: '/static/times-react-demo-still.png',
+            problem: "Most news sites don't provide a clean interface for searching articles and saving them to read at the user's convenience.",
+            solution: "Readers can search for New York Times articles, with an optional date selection, see the results, and save articles for later, all in a snappy clutter-free interface on NYT Clean.",
             technologies: ['/static/react-logo.png', '/static/mongo-logo.png'],
             githubLink: 'https://github.com/bry-an/times-react',
             liveLink: 'https://thawing-fortress-55637.herokuapp.com/',
             id: 'nyt-clean'
         },
-        {
-            name: 'QuickCal',
-            still: 'http://via.placeholder.com/500x281',
-            gif:'',
-            technologies: [],
-            githubLink: '',
-            liveLink: '',
-            id: 'quick-cal'
-        },
+        // {
+        //     name: 'QuickCal',
+        //     still: 'http://via.placeholder.com/500x281',
+        //     gif: '',
+        //     technologies: [],
+        //     githubLink: '',
+        //     liveLink: '',
+        //     id: 'quick-cal'
+        // },
 
     ]
 
